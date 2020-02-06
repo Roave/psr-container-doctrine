4aadc0eac27ff169187073ccb2fab9ad973821f6
M. Yilmaz SUSLU <yilmazsuslu@gmail.com>
2020-02-07 00:24:19 +0100

    Remove unused import

--- src/DriverFactory.php
+++ src/DriverFactory.php
@@ -13,7 +13,6 @@ use Doctrine\Persistence\Mapping\Driver\MappingDriverChain;
 use Psr\Container\ContainerInterface;
 use Roave\PsrContainerDoctrine\Exception\OutOfBoundsException;
 use function array_key_exists;
-use function class_exists;
 use function is_array;
 use function is_subclass_of;
 
