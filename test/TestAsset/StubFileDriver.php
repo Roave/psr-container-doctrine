ea077a939518aa4033aa6956e035bc7068ca6ca8
Tomasz Woźniak <tomasz.wozniak@smartframe.io>
2021-04-22 15:45:46 +0200

    ignore StubFileDriver method

--- test/TestAsset/StubFileDriver.php
+++ test/TestAsset/StubFileDriver.php
@@ -19,6 +19,7 @@ class StubFileDriver extends FileDriver
 
     /**
      * @param string|class-string $className
+     * @psalm-ignore-var
      */
     public function loadMetadataForClass($className, ClassMetadata $metadata): void
     {
